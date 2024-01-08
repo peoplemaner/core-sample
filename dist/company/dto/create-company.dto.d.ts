cad4dfe033620050112b1b8e564469a851b5f4f4
dev-louis-2022 <dev.louis2022@gmail.com>
2024-01-09 00:58:42 +0900

    companies

---
+++ dist/company/dto/create-company.dto.d.ts
@@ -0,0 +1,3 @@
+import { Company } from '../entities/company.entity';
+export declare class CreateCompanyDto extends Company {
+}
---
+++ dist/company/dto/create-company.dto.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.CreateCompanyDto = void 0;
+const company_entity_1 = require("../entities/company.entity");
+class CreateCompanyDto extends company_entity_1.Company {
+}
+exports.CreateCompanyDto = CreateCompanyDto;
+//# sourceMappingURL=create-company.dto.js.map
